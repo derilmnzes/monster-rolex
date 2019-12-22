22eb293ab67caab6b4dc6efa8742247f9e641853
derilmnzes <derilmenezes02@gmail.com>
2019-12-22 05:38:09 -0800

    Updates

---
+++ precache-manifest.bd4cc8eb1c23e97ec878c014a81e673b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d745a5bf56013429a9f0f7775423b65",
+    "url": "/monster-rolex/index.html"
+  },
+  {
+    "revision": "609389e2fe90a34457d6",
+    "url": "/monster-rolex/static/css/main.7170c523.chunk.css"
+  },
+  {
+    "revision": "4425c2ba3c4ab22f674a",
+    "url": "/monster-rolex/static/js/2.2950fb84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolex/static/js/2.2950fb84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "609389e2fe90a34457d6",
+    "url": "/monster-rolex/static/js/main.31dbd7cb.chunk.js"
+  },
+  {
+    "revision": "d1d9afef11475fcd1a6a",
+    "url": "/monster-rolex/static/js/runtime-main.1c51e193.js"
+  }
+]);
